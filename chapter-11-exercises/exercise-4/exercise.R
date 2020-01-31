dea9d3297d3d9fe6af0793c545760325746b756a
Jiatian <jiatix@uw.edu>
2020-01-30 17:20:19 -0800

    week4 exercises

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -2,22 +2,29 @@
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
+#install.packages("nycflights13")
 
+#install.packages("dplyr")
 
+library("nycflights13")
+library("dplyr")
+#install.packages("pkgconfig")
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+??flights
+View(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, delayed_in_air = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
+flights <- arrange(flights, delayed_in_air)
 
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
@@ -29,13 +36,20 @@
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
+summarize(flights, avg = mean(delayed_in_air, na.rm = TRUE))
 
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
+flights %>%
+  filter(dest == "SEA") %>%
+  summarize(avg_delayed = mean(delayed_in_air, na.rm = TRUE)) %>%
+  pull(avg_delayed)
+
 
 
 # On average, did flights to SeaTac gain or loose time?
+summarize(to_sea, avg_delayed = mean(delayed_in_air, na.rm = TRUE))
 
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
